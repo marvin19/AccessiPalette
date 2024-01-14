3286f5f6248a98a50b79c58fcc0c47b3cce9d8bb
marvin19 <marita.vindedal@highsoft.com>
2024-01-14 20:10:52 +0100

    Small text update.

--- src/CardGrid.tsx
+++ src/CardGrid.tsx
@@ -53,6 +53,7 @@ const CardGrid: React.FC = () => {
         <div className="card">
             <h2>The Adjacent Color palette with contrast {selectedContrast === 0 ? '4.5:1' : '3:1'}</h2>
 			<p>This section checks contrast between adjacent colors for clear visualization if stacking colors in e.g charts. <strong>Non-adjacent colors are not compared</strong></p>
+      <p>If you want to keep a color in your color palette, but the neighbor color has not enough contrast. Try drag and drop in section 2.</p>
 			<div className="color-box-container">
             	{renderColorAndContrastBoxes(colors, visibleColors)}
 			</div>
