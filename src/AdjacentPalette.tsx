e856518415be5ac4469728ec6f263c49a42c2a7f
marvin19 <marita.vindedal@highsoft.com>
2024-01-15 14:45:11 +0100

    Refactored adjacent palette into its own component.

---
+++ src/AdjacentPalette.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { renderColorAndContrastBoxes } from './utils'
+
+type AdjacentPaletteProps = {
+    selectedContrast: number;
+    colors: string[];
+    visibleColors: number;
+} 
+
+const AdjacentPalette: React.FC<AdjacentPaletteProps> = ({ selectedContrast, colors, visibleColors}) => {
+   
+  return (
+    <div className="adjacent-palette">
+      <h2>The Adjacent Color palette with contrast {selectedContrast === 4.5 ? '4.5:1' : '3:1'}</h2>
+			<p>This section checks contrast between adjacent colors for clear visualization if stacking colors in e.g charts. <strong>Non-adjacent colors are not compared</strong></p>
+      <p>If you want to keep a color in your color palette, but the neighbor color has not enough contrast. Try drag and drop in section 2.</p>
+			<div className="color-box-container">
+            	{renderColorAndContrastBoxes(colors, visibleColors, selectedContrast)}
+		  </div>
+    </div>
+  )
+}
+
+export default AdjacentPalette
--- src/CardGrid.tsx
+++ src/CardGrid.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import AdjacentPalette from './AdjacentPalette';
 import ColorSelect from './ColorSelect';
 import ContrastSelect from './ContrastSelect';
 import ColorPickerList from './ColorPickerList'
@@ -51,12 +52,7 @@ const CardGrid: React.FC = () => {
             <button className="btn" onClick={handleGeneratePalette}>Generate random colors again</button>
         </div>
         <div className="card">
-            <h2>The Adjacent Color palette with contrast {selectedContrast === 4.5 ? '4.5:1' : '3:1'}</h2>
-			<p>This section checks contrast between adjacent colors for clear visualization if stacking colors in e.g charts. <strong>Non-adjacent colors are not compared</strong></p>
-      <p>If you want to keep a color in your color palette, but the neighbor color has not enough contrast. Try drag and drop in section 2.</p>
-			<div className="color-box-container">
-            	{renderColorAndContrastBoxes(colors, visibleColors, selectedContrast)}
-			</div>
+            <AdjacentPalette colors={colors} selectedContrast={selectedContrast} visibleColors={visibleColors}/>
         </div>
     </div>
   )
