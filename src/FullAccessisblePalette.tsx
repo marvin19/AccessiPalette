f0105379c027e7099b7a94d34f88540e55033258
marvin19 <marita.vindedal@highsoft.com>
2024-01-15 15:33:09 +0100

    Added coming soon.

--- src/FullAccessisblePalette.tsx
+++ src/FullAccessisblePalette.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import SmallColorBox from './SmallColorBox';
 import { renderColorAndContrastBoxes } from './utils'
 
 type FullAccessiblePaletteProps = {
@@ -12,11 +13,7 @@ const FullAccessiblePalette: React.FC<FullAccessiblePaletteProps> = ({ selectedC
   return (
     <div className="adjacent-palette">
       <h2>The Full Accessible palette with contrast {selectedContrast === 4.5 ? '4.5:1' : '3:1'}</h2>
-			<p>This section checks contrast between adjacent colors for clear visualization if stacking colors in e.g charts. <strong>Non-adjacent colors are not compared</strong></p>
-      <p>If you want to keep a color in your color palette, but the neighbor color has not enough contrast. Try drag and drop in section 2.</p>
-			<div className="color-box-container">
-            	{renderColorAndContrastBoxes(colors, visibleColors, selectedContrast)}
-		  </div>
+			<p className="m-2">Coming soon.... </p>
     </div>
   )
 }
