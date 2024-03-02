a2b13275282377ce45e15f64c4fc959af394ac9a
marvin19 <marita.vindedal@highsoft.com>
2024-03-02 16:20:20 +0100

    Removed and added some comments.

--- src/components/FindColor.tsx
+++ src/components/FindColor.tsx
@@ -10,7 +10,6 @@ interface FindColorProps {
     paletteType?: string;
 }
 
-// adding a comment to create a PR
 const FindColor: React.FC<FindColorProps> = ({ colors, selectedContrast }) => {
     const [contrastColor, setContrastColor] = useState<string | null>(null);
 
@@ -25,15 +24,14 @@ const FindColor: React.FC<FindColorProps> = ({ colors, selectedContrast }) => {
             <Arrow direction="right" />
             <div style={{ textAlign: 'center' }}>
                 <ColorBox color={contrastColor ?? 'transparent'} />
+                {/* TODO: Create a component for this when UI is decided on */}
                 {contrastColor !== null ? (
                     <p>Contrast color: {contrastColor}</p>
                 ) : (
                     <p>No colors available with enough contrast.</p>
                 )}
             </div>
-            <div className="flex items-center justify-center">
-                <Arrow direction="left" />
-            </div>
+            <Arrow direction="left" />
             <ColorBox color={colors[1]} />
         </div>
     );
