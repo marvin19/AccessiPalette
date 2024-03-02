abebd2136b444beb73a1b87032acf6452b43b447
marvin19 <marita.vindedal@highsoft.com>
2024-03-02 23:22:13 +0100

    Added unit-test for arrow.

---
+++ test/Arrow.test.tsx
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+import Arrow from '../src/components/Arrow';
+
+jest.mock('@fortawesome/react-fontawesome', () => ({
+    FontAwesomeIcon: jest.fn(() => <div>Mock Icon</div>),
+}));
+
+describe('Arrow', () => {
+    it('renders the correct icon for the right direction', () => {
+        const { getByText } = render(<Arrow direction="right" />);
+        expect(getByText('Mock Icon')).toBeInTheDocument();
+    });
+});
