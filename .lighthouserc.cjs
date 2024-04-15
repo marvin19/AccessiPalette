2c5a6b877c19b4f9ce7feae39e9171e0fc28ecf9
marvin19 <marita.vindedal@highsoft.com>
2024-04-15 10:48:07 +0200

    Tested previous workflow and config file.

--- .lighthouserc.cjs
+++ .lighthouserc.cjs
@@ -3,7 +3,6 @@ module.exports = {
         collect: {
             staticDistDir: './dist',
             url: ['http://localhost:4173'],
-            numberOfRuns: 1,
         },
         assert: {
             assertions: {
@@ -11,9 +10,7 @@ module.exports = {
             },
         },
         upload: {
-            target: 'filesystem',
-            outputDir: './lighthouse-results', // Define where to store the results
-            reportFilenamePattern: 'results-%%SUFFIX%%.json', // This will save the file as results-0.json, results-1.json, etc.
+            target: 'temporary-public-storage',
         },
     },
 };
