4611cd0d540c20dc3c297b5000f3f8d1559f4940
marvin19 <marita.vindedal@highsoft.com>
2024-04-15 15:25:41 +0200

    Test.

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,9 +1,9 @@
 const Heading = (): JSX.Element => {
     return (
         <div className="container w-full mx-auto bg-">
-            <h2 className="h1">
+            <h1 className="h1">
                 <span aria-hidden="true">🎨</span> AccessiPalette
-            </h2>
+            </h1>
         </div>
     );
 };
