d3be38c1f3838393c3d54e4eba2703873d80ab3f
marvin19 <marita.vindedal@highsoft.com>
2024-07-10 14:49:59 +0200

    Added ColorBarList and ColorBar components.

--- src/components/ColorBar.tsx
+++ src/components/ColorBar.tsx
@@ -1,5 +1,30 @@
-const ColorBar = (): JSX.Element => {
-    return <div className="color-bar-outer">ColorBar</div>;
+import React from 'react';
+import closeButton from '../assets/x-close.svg';
+
+interface ColorBarProps {
+    onRemove: () => void;
+    color: string;
+}
+
+const ColorBar: React.FC<ColorBarProps> = ({
+    onRemove,
+    color,
+}): JSX.Element => {
+    return (
+        <div className="color-bar-outer" style={{ backgroundColor: color }}>
+            <div className="color-bar-inner">
+                <div className="color-bar-inner-inner">
+                    <div className="contrast-text-container">
+                        <p className="contrast-text">4.5:1</p>
+                    </div>
+                    <p className="contrast-text-label">Text Contrast Ratio</p>
+                </div>
+            </div>
+            <button className="remove-color" onClick={onRemove}>
+                <img src={closeButton} className="close-button" alt="Remove" />
+            </button>
+        </div>
+    );
 };
 
 export default ColorBar;
--- src/components/ColorBarList.tsx
+++ src/components/ColorBarList.tsx
@@ -1,9 +1,47 @@
+import { useState } from 'react';
 import ColorBar from './ColorBar';
 
+const defaultColors = [
+    '#6975ff',
+    '#084fd7',
+    '#64bdc6',
+    '#eeca34',
+    '#fe7135',
+    '#fd2a2a',
+];
+
 const ColorBarList = (): JSX.Element => {
+    const [colorBars, setColorBars] = useState<string[]>(defaultColors);
+
+    const addColorBar = (): void => {
+        if (colorBars.length < 10) {
+            const newColor = '#000000';
+            setColorBars([...colorBars, newColor]);
+        }
+    };
+
+    const removeColorBar = (index: number): void => {
+        if (colorBars.length > 2) {
+            setColorBars(colorBars.filter((_, i) => i !== index));
+        }
+    };
+
     return (
         <div className="color-bars">
-            <ColorBar />
+            {colorBars.map((color, index) => (
+                <ColorBar
+                    key={index}
+                    color={color}
+                    onRemove={() => {
+                        removeColorBar(index);
+                    }}
+                />
+            ))}
+            {colorBars.length < 10 && (
+                <button onClick={addColorBar} className="add-color-bar">
+                    + Add color bar
+                </button>
+            )}
         </div>
     );
 };
