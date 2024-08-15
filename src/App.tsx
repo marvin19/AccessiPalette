917f06e596d6260445686086579fe42e68d60808
marvin19 <marita.vindedal@highsoft.com>
2024-08-15 12:23:51 +0200

    Removed toolbar and implemented drag and drop for all.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
 import Header from './components/Header';
 import Palette from './components/Palette';
-import Toolbar from './components/Toolbar';
 
 const App: React.FC = (): React.ReactElement | null => {
     const [selectedContrast, setSelectedContrast] = useState<number>(3.0);
     const [selectedMode, setSelectedMode] = useState<string>('neighbor');
-    const [enableDragAndDrop, setEnableDragAndDrop] = useState<boolean>(false);
 
     return (
         <main>
@@ -14,14 +12,9 @@ const App: React.FC = (): React.ReactElement | null => {
                 setSelectedContrast={setSelectedContrast}
                 setSelectedMode={setSelectedMode}
             />
-            <Toolbar
-                enableDragAndDrop={enableDragAndDrop}
-                setEnableDragAndDrop={setEnableDragAndDrop}
-            />
             <Palette
                 selectedContrast={selectedContrast}
                 selectedMode={selectedMode}
-                enableDragAndDrop={enableDragAndDrop}
             />
         </main>
     );
